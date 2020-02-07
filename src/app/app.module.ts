d7a1bf2bcc3fec45521b5c5d4ce7a1126e639f3f
pablohmartin <hermar65@gmail.com>
2020-02-07 11:53:39 +0100

    feat: analytics configurado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { SwiperModule } from 'ngx-swiper-wrapper';
 import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
 import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
+import { AngularFireAnalyticsModule, CONFIG, ScreenTrackingService } from '@angular/fire/analytics';
+
 
 const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
   direction: 'horizontal',
@@ -36,6 +38,7 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
     AngularFirestoreModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -44,10 +47,16 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
+    ScreenTrackingService,
     {
       provide: SWIPER_CONFIG,
       useValue: DEFAULT_SWIPER_CONFIG
-    }
+    },
+     { provide: CONFIG, useValue: {
+      send_page_view: false,
+      allow_ad_personalization_signals: false,
+      anonymize_ip: true
+    } }
   ],
   bootstrap: [AppComponent]
 })
